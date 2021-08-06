30ded57711948fa03108a5309ee6859711b1cc14
tianmeide <tianmeide@126.com>
2021-08-06 11:11:14 +0800

    增加了测试代码

--- interview/src/chapter/two/fifteenth/Conver1.java
+++ interview/src/chapter/two/fifteenth/Conver1.java
@@ -10,7 +10,7 @@ import java.util.Queue;
  */
 public class Conver1 {
 
-	public Node convert1(Node head) {
+	public static Node convert1(Node head) {
 		Queue<Node> queue = new LinkedList<Node>();
 		inOrderToQueue(head, queue);
 		if (queue.isEmpty()) {
@@ -30,7 +30,7 @@ public class Conver1 {
 		return head;
 	}
 	
-	public void inOrderToQueue(Node head, Queue<Node> queue) {
+	public static void inOrderToQueue(Node head, Queue<Node> queue) {
 		if (head == null) {
 			return;
 		}
@@ -38,9 +38,55 @@ public class Conver1 {
 		queue.offer(head);
 		inOrderToQueue(head.right, queue);
 	}
+	
+	public static void printBSTInOrder(Node head) {
+		System.out.print("BST in-order:");
+		if (head != null) {
+			inOrderPrint(head);
+		}
+		System.out.println();
+	}
+	
+	public static void inOrderPrint(Node head) {
+		if (head == null) {
+			return;
+		}
+		inOrderPrint(head.left);
+		System.out.print(head.value + " ");
+		inOrderPrint(head.right);
+	}
+	public static void printDoubleLinkedList(Node head) {
+		System.out.print("Double Linked List:");
+		Node end = null;
+		while (head != null) {
+			System.out.print(head.value + " ");
+			end = head;
+			head = head.right;	
+		}
+		System.out.print("| ");
+		while (end != null) {
+			System.out.print(end.value + " ");
+			end = end.left;
+		}
+		System.out.println();
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Node head = new Node(5);
+		head.left = new Node(2);
+		head.right = new Node(9);
+		head.left.left = new Node(1);
+		head.left.right = new Node(3);
+		head.left.right.right = new Node(4);
+		head.right.left = new Node(7);
+		head.right.right = new Node(10);
+		head.left.left = new Node(1);
+		head.right.left.left = new Node(6);
+		head.right.left.right = new Node(8);
+		printBSTInOrder(head);
+		head = convert1(head);
+		printDoubleLinkedList(head);
 	}
 
+	
 }
--- interview/src/chapter/two/fifteenth/Conver2.java
+++ interview/src/chapter/two/fifteenth/Conver2.java
@@ -7,7 +7,7 @@ package chapter.two.fifteenth;
  */
 public class Conver2 {
 
-	public Node convert2(Node head) {
+	public static Node convert2(Node head) {
 		if (head == null) {
 			return null;
 		}
@@ -17,7 +17,7 @@ public class Conver2 {
 		return head;
 	}
 	
-	public Node process(Node head) {
+	public static Node process(Node head) {
 		if (head == null) {
 			return null;
 		}
@@ -47,9 +47,57 @@ public class Conver2 {
 			return head;
 		}
 	}
+	
+	public static void printBSTInOrder(Node head) {
+		System.out.print("BST in-order: ");
+		if (head != null) {
+			inOrderPrint(head);
+		}
+		System.out.println();
+	}
+	
+	public static void inOrderPrint(Node head) {
+		if (head == null) {
+			return;
+		}
+		inOrderPrint(head.left);
+		System.out.print(head.value + " ");
+		inOrderPrint(head.right);
+	}
+	
+	public static void printDoubleLinkedList(Node head) {
+		System.out.print("Double Linked List: ");
+		Node end = null;
+		while (head != null) {
+			System.out.print(head.value + " ");
+			end = head;
+			head = head.right;
+		}
+		System.out.print("| ");
+		while (end != null) {
+			System.out.print(end.value + " ");
+			end = end.left;
+		}
+		System.out.println();
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Node head = new Node(5);
+		head.left = new Node(2);
+		head.right = new Node(9);
+		head.left.left = new Node(1);
+		head.left.right = new Node(3);
+		head.left.right.right = new Node(4);
+		head.right.left = new Node(7);
+		head.right.right = new Node(10);
+		head.left.left = new Node(1);
+		head.right.left.left = new Node(6);
+		head.right.left.right = new Node(8);
+		
+		printBSTInOrder(head);
+		head = convert2(head);
+		printDoubleLinkedList(head);
 	}
 
 }
