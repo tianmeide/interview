7776f3c31668e58101f68daf383ce4a421ac80f7
tianmeide <tianmeide@126.com>
2021-08-11 17:45:39 +0800

    添加测试代码

--- interview/src/chapter/two/seventeenth/RemoveNodeWire.java
+++ interview/src/chapter/two/seventeenth/RemoveNodeWire.java
@@ -20,13 +20,32 @@ public class RemoveNodeWire {
 		node.value = next.value;
 		node.next = next.next;
 	}
-	
+	public static void printLinkedList(Node head) {
+		System.out.print("Linked list:");
+		while (head != null) {
+			System.out.print(head.value + " ");
+			head = head.next;
+		}
+		System.out.println();
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Node node = new Node(1);
-		node.next = new Node(2);
-		node.next.next = new Node(3);
+		Node head = new Node(1);
+		head.next = new Node(2);
+		head.next.next = new Node(3);
+		Node node = head.next;
+		printLinkedList(head);
 		removeNodeWired(node);
+		printLinkedList(head);
+		
+		head = new Node(1);
+		head.next = new Node(2);
+		head.next.next = new Node(3);
+		node = head.next;
+		printLinkedList(head);
+		removeNodeWired(node);
+		printLinkedList(head);
+		
 	}
 
 }
